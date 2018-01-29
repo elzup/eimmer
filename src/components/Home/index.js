2f9ea927c62d409f333c8a6e8862741cb8510ca9
elzup <guild0105@gmail.com>
2018-01-30 06:45:23 +0900

    Display point

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,40 +1,69 @@
 // @flow
 
 import React, { Component } from 'react'
-import { Stage, Layer, Rect, Text } from 'react-konva'
+import { Stage, Layer, Circle, Text } from 'react-konva'
 
-class ColoredRect extends React.Component {
-	state = {
-		color: 'green',
-	}
-	handleClick = () => {
-		this.setState({
-			color: 'red',
-		})
-	}
-	render() {
-		return (
-			<Rect
-				x={20}
-				y={20}
-				width={50}
-				height={50}
-				fill={this.state.color}
-				shadowBlur={5}
-				onClick={this.handleClick}
-			/>
-		)
-	}
+type HitResult = {
+	point: number,
 }
 
-class App extends Component {
+const Target = (props: {
+	r: number,
+	x: number,
+	y: number,
+	point: number,
+	handleHit: HitResult => void,
+}) => {
+	return (
+		<Layer>
+			<Text
+				x={props.x - 5}
+				y={props.y - props.r * 2.5}
+				fontSize={props.r}
+				text={props.point.toString()}
+			/>
+			<Circle
+				x={props.x}
+				y={props.y}
+				radius={props.r}
+				fill={'green'}
+				shadowBlur={5}
+				onClick={({ evt }: { evt: { x: number, y: number } }) => {
+					const dx = props.x - evt.x
+					const dy = props.y - evt.y
+					const d = Math.sqrt(dx * dx + dy * dy)
+					const rate = d / props.r
+					const point = 10 - Math.floor(rate * 10)
+					props.handleHit({ point })
+				}}
+			/>
+		</Layer>
+	)
+}
+
+type Props = {}
+type State = {
+	point: number,
+}
+
+class App extends Component<Props, State> {
+	state = {
+		point: 0,
+	}
 	render() {
+		const { state } = this
 		return (
 			<Stage width={window.innerWidth} height={window.innerHeight}>
-				<Layer>
-					<Text text="Try click on rect" />
-					<ColoredRect />
-				</Layer>
+				<Target
+					r={25}
+					x={window.innerWidth / 2}
+					y={window.innerHeight / 2}
+					point={state.point}
+					handleHit={(e: HitResult) => {
+						console.log(e)
+						this.setState({ point: e.point })
+					}}
+				/>
 			</Stage>
 		)
 	}
