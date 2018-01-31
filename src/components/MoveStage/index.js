be0a9e9feed28cfb400fa520bf3d5d8ba34570fc
elzup <guild0105@gmail.com>
2018-01-31 16:43:39 +0900

    Move Repeat motion

--- src/components/MoveStage/index.js
+++ src/components/MoveStage/index.js
@@ -2,9 +2,9 @@
 
 import React, { Component } from 'react'
 import { Stage } from 'react-konva'
-import type { TargetItem } from '../../types'
 import Target, { type HitResult } from '../Target'
-import { Motion, spring } from 'react-motion'
+import { spring } from 'react-motion'
+import { ReactMotionLoop } from 'react-motion-loop'
 
 type Props = {}
 type State = {
@@ -27,50 +27,28 @@ class MoveStage extends Component<Props, State> {
 		window.addEventListener('resize', this.updateDimensions)
 	}
 
-	/**
-	 * Remove event listener
-	 */
 	componentWillUnmount() {
 		window.removeEventListener('resize', this.updateDimensions)
 	}
 
 	render() {
-		const targets: TargetItem[] = [
-			{ x: 0.5, y: 0.5, r: 25 },
-			{ x: 0.2, y: 0.2, r: 25 },
-			{ x: 0.8, y: 0.8, r: 25 },
-			{ x: 0.2, y: 0.8, r: 25 },
-			{ x: 0.8, y: 0.2, r: 25 },
-		]
 		return (
 			<Stage width={window.innerWidth} height={window.innerHeight}>
-				<Motion
-					defaultStyle={{ dx: 0.2 }}
-					onRest={() => {}}
-					style={{ dx: spring(0.8, { stiffness: 30, damping: 20 }) }}
+				<ReactMotionLoop
+					styleFrom={{ dx: spring(0.2, { stiffness: 5, damping: 5 }) }}
+					styleTo={{ dx: spring(0.8, { stiffness: 5, damping: 5 }) }}
 				>
-					{ips => (
+					{pos => (
 						<Target
 							r={25}
-							x={window.innerWidth * ips.dx}
+							x={window.innerWidth * pos.dx}
 							y={window.innerHeight * 0.2}
 							handleHit={(e: HitResult) => {
 								console.log(e)
 							}}
 						/>
 					)}
-				</Motion>
-				{targets.map((target, i) => (
-					<Target
-						key={i}
-						r={target.r}
-						x={window.innerWidth * target.x}
-						y={window.innerHeight * target.y}
-						handleHit={(e: HitResult) => {
-							console.log(e)
-						}}
-					/>
-				))}
+				</ReactMotionLoop>
 			</Stage>
 		)
 	}
