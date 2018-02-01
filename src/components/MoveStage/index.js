3631cd52d61136a842cb2e0b3ade3998338aadb8
elzup <guild0105@gmail.com>
2018-02-01 13:18:58 +0900

    Add span move target

--- src/components/MoveStage/index.js
+++ src/components/MoveStage/index.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react'
 import Animate from 'react-move/Animate'
+import { Layer, Line } from 'react-konva'
 import { easeSinInOut } from 'd3-ease'
 
 import { Stage } from 'react-konva'
@@ -12,6 +13,7 @@ type State = {
 	w: number,
 	h: number,
 	dx: number,
+	dx2: number,
 }
 
 class MoveStage extends Component<Props, State> {
@@ -19,6 +21,7 @@ class MoveStage extends Component<Props, State> {
 		w: window.innerWidth,
 		h: window.innerHeight,
 		dx: 0.2,
+		dx2: 0.2,
 	}
 
 	updateDimensions = () => {
@@ -35,8 +38,16 @@ class MoveStage extends Component<Props, State> {
 	}
 
 	render() {
+		const { state: { h, w } } = this
 		return (
-			<Stage width={window.innerWidth} height={window.innerHeight}>
+			<Stage width={w} height={h}>
+				<Layer>
+					<Line
+						stroke={'#000'}
+						points={[w * 0.2, h * 0.2, w * 0.8, h * 0.2]}
+						fill={'black'}
+					/>
+				</Layer>
 				<Animate
 					start={() => ({
 						x: 0.2,
@@ -50,10 +61,42 @@ class MoveStage extends Component<Props, State> {
 						return (
 							<Target
 								r={25}
-								x={window.innerWidth * state.x}
-								y={window.innerHeight * 0.2}
+								x={w * state.x}
+								y={h * 0.2}
 								handleHit={(e: HitResult) => {
 									this.setState({ dx: 0.2 + Math.random() * 0.6 })
+									console.log(e)
+								}}
+							/>
+						)
+					}}
+				</Animate>
+				<Layer>
+					<Line
+						stroke={'#000'}
+						points={[w * 0.2, h * 0.4, w * 0.8, h * 0.4]}
+						fill={'black'}
+					/>
+				</Layer>
+				<Animate
+					start={() => ({
+						x: 0.2,
+					})}
+					update={() => ({
+						x: [this.state.dx2],
+						timing: { duration: 200, ease: easeSinInOut },
+					})}
+				>
+					{state => {
+						return (
+							<Target
+								r={25}
+								x={w * state.x}
+								y={h * 0.4}
+								handleHit={(e: HitResult) => {
+									const dx2 =
+										this.state.dx2 + 0.1 > 0.8 ? 0.2 : this.state.dx2 + 0.1
+									this.setState({ dx2 })
 									console.log(e)
 								}}
 							/>
